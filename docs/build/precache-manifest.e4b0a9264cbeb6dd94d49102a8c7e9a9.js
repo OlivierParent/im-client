cd1de633553e03e0a08e93ab81331fc9febc6da4
OlivierParent <olivier.parent@arteveldehs.be>
2020-10-18 15:25:55 +0200

    [WIP]

---
+++ docs/build/precache-manifest.e4b0a9264cbeb6dd94d49102a8c7e9a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fff36062ea00df6b585339f3d900baf",
+    "url": "/im-client/index.html"
+  },
+  {
+    "revision": "8f7f720042fe5b3103a1",
+    "url": "/im-client/static/css/main.d866904c.chunk.css"
+  },
+  {
+    "revision": "2c10caf903011a05f01f",
+    "url": "/im-client/static/js/2.a2edd36d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/im-client/static/js/2.a2edd36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7f720042fe5b3103a1",
+    "url": "/im-client/static/js/main.c62b964b.chunk.js"
+  },
+  {
+    "revision": "7f5ec064387eae34e754",
+    "url": "/im-client/static/js/runtime-main.75549f0a.js"
+  },
+  {
+    "revision": "ce019e05cd7b57125c2c009f35c43032",
+    "url": "/im-client/static/media/glTF.ce019e05.glb"
+  },
+  {
+    "revision": "fdd6049581f7de1413dc09ef19660491",
+    "url": "/im-client/static/media/suzanne.fdd60495.glb"
+  }
+]);
